18874a3b3743d3a8d74757d7ed701b77b39047d7
Guangchuang Yu <guangchuangyu@gmail.com>
2023-12-29 23:36:33 +0800

    allow un-quoted word in ydict()

--- R/dictionary.r
+++ R/dictionary.r
@@ -3,8 +3,11 @@
 ##' @title ydict
 ##' @param word word to check
 ##' @return interpretation from youdao dictionary
+##' @importFrom rlang as_name
+##' @importFrom rlang enquo
 ##' @export
 ydict <- function(word) {
+    word <- rlang::as_name(rlang::enquo(word))
     x <- .youdao_translate(word) 
     y <- sprintf("\n\tUK: [%s]\tUS: [%s]\n\n\tExplains: %s\n\n\tWeb: %s\n\n", 
             x$basic$`uk-phonetic`, 
