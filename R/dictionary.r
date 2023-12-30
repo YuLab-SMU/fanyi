e6a69bc09d5b372fe73328c350b426b2cbe8d2ba
Guangchuang Yu <guangchuangyu@gmail.com>
2023-12-30 17:26:43 +0800

    whether open web browser

--- R/dictionary.r
+++ R/dictionary.r
@@ -2,23 +2,28 @@
 ##'
 ##' @title ydict
 ##' @param word word to check
+##' @param web whether open youdao dict in web browser
 ##' @return interpretation from youdao dictionary
 ##' @importFrom rlang as_name
 ##' @importFrom rlang enquo
 ##' @export
-ydict <- function(word) {
+ydict <- function(word, web=FALSE) {
     word <- rlang::as_name(rlang::enquo(word))
     x <- .youdao_translate(word) 
 
+    url <- x$mTerminalDict$url
     uk <- yd_format_item("\tUK: [%s]", x$basic$`uk-phonetic`)
     us <- yd_format_item("\tUS: [%s]", x$basic$`us-phonetic`)
     explain <- yd_format_item("\tExplains: %s", x$basic$explains[[1]])
-    web <- yd_format_item("\tWeb: %s", x$mTerminalDict$url)
+    www <- yd_format_item("\tWeb: %s", url)
 
     y <- sprintf("\n%s%s\n\n%s\n\n%s\n\n", 
-                    uk, us, explain, web)
+                    uk, us, explain, www)
 
     cat(y)
+    if (web && !is.null(url)) {
+        utils::browseURL(url)
+    }
 }
 
 yd_format_item <- function(format, item) {
